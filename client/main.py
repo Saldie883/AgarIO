699633f46bd9dd8da70687ad17476c3ac821e407
Sasha Gorbar <57323430+Saldie883@users.noreply.github.com>
2022-01-16 19:33:41 +0200

    Creating the main menu

--- client/main.py
+++ client/main.py
@@ -1,21 +1,47 @@
+# Библиотеки
+import sys 
 import pygame
+import math
+from pygame.locals import *
+import pygame_menu
+
 pygame.init()
 
-defaultSize = 24
+# Константы
+DISPLAY_WIDTH   = 1280
+DISPLAY_HEIGHT  = 720
 
-screen = pygame.display.set_mode([640, 480])
+# Переменные
+mainScreen = pygame.display.set_mode([DISPLAY_WIDTH, DISPLAY_HEIGHT])
+pygame.display.set_caption("Agar.io")
 
-running = True
-while running:
+mainClock = pygame.time.Clock() 
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# Меню
+menu  = pygame_menu.Menu('Agar.io', DISPLAY_WIDTH, DISPLAY_HEIGHT, theme=pygame_menu.themes.THEME_BLUE)
 
-    screen.fill((255, 255, 255))
+def start_the_game():
+    global gameState, menu 
+    name = userNameTextInput.get_value()
+    if len(name) < 4:
+        print("Имя слишком короткое")
+    else:
+        menu.disable()
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), defaultSize)
+userNameTextInput = menu.add.text_input('Name :', default='Tester')
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+while (True):
+    mainClock.tick(60)
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT:
+            pygame.quit()
+
+    mainScreen.fill((255, 255, 255))
+
+    if menu.is_enabled():
+        menu.mainloop(mainScreen)
 
     pygame.display.flip()
-
-pygame.quit()
