e3662a1ad5acecbc1b49654c6e0919ad7fd4bf25
Sasha Gorbar <57323430+Saldie883@users.noreply.github.com>
2022-01-16 22:03:42 +0200

    Creating a player class

--- client/main.py
+++ client/main.py
@@ -18,6 +18,9 @@ pygame.display.set_caption("Agar.io")
 
 mainClock = pygame.time.Clock() 
 
+font        = pygame.font.SysFont('Ubuntu',20,True)
+big_font    = pygame.font.SysFont('Ubuntu',24,True)
+
 # Меню
 menu = pygame_menu.Menu('Agar.io', DISPLAY_WIDTH, DISPLAY_HEIGHT, theme=pygame_menu.themes.THEME_BLUE)
 
@@ -33,6 +36,11 @@ userNameTextInput = menu.add.text_input('Name :', default='Tester')
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
+
+def getDistance(a, b):
+    diffX = math.fabs(a[0]-b[0])
+    diffY = math.fabs(a[1]-b[1])
+    return ((diffX**2)+(diffY**2))**(0.5)
 
 
 class Camera:
@@ -80,8 +88,7 @@ class Grid(Drawable):
 
 class Cell(Drawable): 
     """ Класс клеток которые игроки поднимают """
-    CELL_COLORS = 
-    [ # Цвета для клеток
+    CELL_COLORS = [ # Цвета для клеток
         (80,252,54),
         (36,244,255),
         (243,31,46),
@@ -120,24 +127,103 @@ class CellList(Drawable):
         for cell in self.list:
             cell.draw()
 
+
+class Player(Drawable):
+    """ Класс игрока
+    """
+    COLOR_LIST = [
+    (37,7,255),
+    (35,183,253),
+    (48,254,241),
+    (19,79,251),
+    (255,7,230),
+    (255,7,23),
+    (6,254,13)]
+
+    FONT_COLOR = (50, 50, 50)
+    
+    def __init__(self, surface, camera, name = ""):
+        super().__init__(surface, camera)
+        self.x = random.randint(100,400)
+        self.y = random.randint(100,400)
+        self.mass = 20
+        self.speed = 4
+        self.color = col = random.choice(Player.COLOR_LIST)
+        self.outlineColor = (
+            int(col[0]-col[0]/3),
+            int(col[1]-col[1]/3),
+            int(col[2]-col[2]/3))
+        if name: self.name = name
+        else: self.name = "Test"
+        self.pieces = []
+
+
+    def collisionDetection(self, edibles):
+        """ Проверка коллизии
+        """
+        for edible in edibles:
+            if(getDistance((edible.x, edible.y), (self.x,self.y)) <= self.mass/2):
+                self.mass+=0.5
+                edibles.remove(edible)
+
+
+    def move(self):
+        """ Обновление позиции игрока
+        """
+    
+        dX, dY = pygame.mouse.get_pos()
+        rotation = math.atan2(dY - float(DISPLAY_HEIGHT)/2, dX - float(DISPLAY_WIDTH)/2)
+        rotation *= 180/math.pi
+        normalized = (90 - math.fabs(rotation))/90
+        vx = self.speed*normalized
+        vy = 0
+        if rotation < 0:
+            vy = -self.speed + math.fabs(vx)
+        else:
+            vy = self.speed - math.fabs(vx)
+        tmpX = self.x + vx
+        tmpY = self.y + vy
+        self.x = tmpX
+        self.y = tmpY
+
+    def draw(self):
+        """ Отрисовка игрока
+        """
+        zoom = self.camera.zoom
+        x, y = self.camera.x, self.camera.y
+        center = (int(self.x*zoom + x), int(self.y*zoom + y))
+        
+        # Отрисовка круга вокруг игрока
+        pygame.draw.circle(self.surface, self.outlineColor, center, int((self.mass/2 + 3)*zoom))
+        # Отрисовка круга игрока
+        pygame.draw.circle(self.surface, self.color, center, int(self.mass/2*zoom))
+
+
+
 cam = Camera()
 
 grid = Grid(mainScreen, cam)
 cells = CellList(mainScreen, cam, 2000)
+player = Player(mainScreen, cam, "Test")
 
-while (True):
+
+while True:
     mainClock.tick(60)
     events = pygame.event.get()
     for event in events:
         if event.type == QUIT:
             pygame.quit()
-
-    mainScreen.fill((255, 255, 255))
+            quit()
 
     if menu.is_enabled():
         menu.mainloop(mainScreen)
     else:
+        player.move()
+        player.collisionDetection(cells.list)
+        cam.update(player)
+        mainScreen.fill((255, 255, 255))
         grid.draw()
         cells.draw()
+        player.draw()
 
     pygame.display.flip()
