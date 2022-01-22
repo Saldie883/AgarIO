9342fd9280e81fa9b27ca223a786c5c7fd6bbec2
Sasha Gorbar <57323430+Saldie883@users.noreply.github.com>
2022-01-22 12:28:32 +0200

    client-server communication, data synchronization

--- server.py
+++ server.py
@@ -1,66 +1,95 @@
 import socket
-import pickle # Для сериализации
+import pickle
+from copy import deepcopy
+import threading
+import sys
+import random
+from os import system
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      
+import codes as code
 
-udp_host = "127.0.0.1"	       
-udp_port = 1234			              
 
-sock.bind((udp_host,udp_port))
+def recv_all_data(server, players_data, cells_data):
+    print("Recieving...")
 
-connections = []
+    # player data:
+    #   { pos = (x, y)
+    #     color = (r, g, b)
+    #     mass = int }
 
-def send_request(addr, requestType, requestMessage):
-    data = {
-        "requestType": requestType,
-        "requestMessage": requestMessage
-    }
-    try:
-        sock.sendto(pickle.dumps(data), addr)
-    except Exception as ex:
-        print(ex)
-    return 
-
-def make_addr(connection):
-    return str(connection[0]) + ":" + str(connection[1])
-
-def from_addr(addr):
-    addr = addr.split(":")
-    connection = ""
-    try:
-        connection = (addr[0], int(addr[1]))
-    except Exception as ex:
-        print(ex)
-    return connection
-
-def handle_request(data, addr):
-    data = pickle.loads(data)
-    print("Server: ", data)
-    requestType = data["requestType"]
-    requestMessage = data["requestMessage"]
-    if requestType == 1:
-        connection = make_addr(addr)
-        data["requestMessage"].append(connection)
-        send_request(addr, 3, connection)
-        for i in connections:
-            send_request(from_addr(i), 1, requestMessage)
-        connections.append(connection)
-    elif requestType == 2:
-        addr = requestMessage[8]
-        send_request(from_addr(addr), 2, requestMessage)
-    
-    # 3 skip
-    elif requestType == 4:
-        addr = requestMessage[0]
-        for i in connections:
-            if i != addr:
-                send_request(from_addr(i), 4, requestMessage)
-
-def main():
     while True:
-        print ("Waiting for client...")
-        data, addr = sock.recvfrom(1024)
-        handle_request(data, addr)
+        data, addr = recieve(server)
 
-if __name__ == "__main__":
-    main()
+        if data['code'] == code.CONNECT_REQUEST:
+            new_player_pos = (random.randint(0, MAPSIZE), random.randint(0, MAPSIZE))
+            new_player_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+            players_data[addr] = {'pos': new_player_pos, 'color': new_player_color, 'mass': 20}
+
+            print(f"Connected {addr}")
+            send({'code': code.CONNECTED, 'addr': addr, 'cells': cells_data, 'players': players_data, \
+                'mapsize': MAPSIZE, 'coords': new_player_pos, 'color': new_player_color}, addr)
+
+        elif data['code'] == code.DATA_SEND:
+            players_data[addr] = data
+
+        elif data['code'] == code.CELL_EAT:
+            del cells_data[data['cell']]
+            print(f"got eaten cell: {data['cell']} ({len(cells_data)})")
+
+
+def sync_data(players_data, cells_data):
+    print("Sync started...")
+
+    while True:
+        cd = deepcopy(cells_data)
+        pd = deepcopy(players_data)
+
+        for p in players_data.copy():
+            send({"code": code.DATA_SEND, "cells": cd, "players": pd}, p)
+
+
+def send(data, client):
+    server.sendto(pickle.dumps(data), client)
+
+
+def recieve(server):
+    data, addr = server.recvfrom(2048)
+    return (pickle.loads(data), addr)
+
+
+def make_cell(mapsize):
+    return ((random.randint(10, mapsize-10), random.randint(10, mapsize-10)), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+
+
+HOST, PORT = 'localhost', 7777
+GAMEEND = False
+
+addr = (HOST, PORT)
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(addr)
+
+MAPSIZE = 2000
+CELLS_AMOUNT = MAPSIZE//20
+CELLS_DATA = dict()
+
+for _ in range(CELLS_AMOUNT):
+    new_cell = make_cell(MAPSIZE)
+
+    CELLS_DATA[new_cell[0]] = new_cell[1]
+
+system("CLS")
+# -------------------------------------------------------
+
+print("\t\t[ Server started ]")
+
+PLAYERS_DATA = {}
+recv_thread = threading.Thread(target=recv_all_data, args=(server, PLAYERS_DATA, CELLS_DATA))
+send_thread = threading.Thread(target=sync_data, args=(PLAYERS_DATA, CELLS_DATA))
+
+
+recv_thread.start()
+send_thread.start()
+
+if GAMEEND:
+    server.close()
